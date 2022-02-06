55dbe33bf4ec808b814bceb301c03f441901d7c1
Sandipan Dey <hey@sandipan.dev>
2022-02-06 17:34:36 +0530

    feat: add card to first page (#7)
    * feat: added basic card
    
    * hmm
    
    * fix: fonts
    
    * feat: complete card basic layout done
    
    * feat: backdrops added
    
    * feat: completed footer
    
    * feat: added hi animation

---
+++ components/Social.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next'
+import { ReactNode } from 'react'
+
+interface PageProps {
+  icon: ReactNode;
+  href: string;
+  tooltipText: string;
+  left?: number;
+}
+
+const Social: NextPage<PageProps> = ({
+  icon,
+  href,
+  tooltipText,
+  left = 40,
+}) => {
+  return (
+    <a className="transition ease-in-out duration-300 hover:scale-110 has-tooltip z-10" href={href} target="_blank" rel="noreferrer">
+      <span
+        className="tooltip bg-black font-mono text-xs bg-opacity-80 text-white rounded p-1 px-2 -mt-8"
+        style={{ marginLeft: `-${left}px` }}
+      >{tooltipText}</span>
+      {icon}
+    </a>
+  );
+}
+
+export default Social
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,27 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./sections/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: "#91FF80"
+      },
+      fontFamily: {
+        body: ['Nunito'],
+        sans: ['"Open Sans"']
+      },
+      animation: {
+        hi: 'hi 1s ease-in-out infinite'
+      },
+      keyframes: {
+        hi: {
+          '0%, 100%': { transform: 'rotate(-8deg)' },
+          '50%': { transform: 'rotate(12deg)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
