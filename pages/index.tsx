df0cc0e84817b5bfc9b8beef0e9eed6254879ea1
Sandipan Dey <hey@sandipan.dev>
2022-02-11 21:13:01 +0530

    feat!: removing other sections for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,11 @@
 import type { NextPage } from 'next'
 
 import Intro from '../sections/Intro'
-import Projects from '../sections/Projects'
-import Experience from '../sections/Experience'
-import Outro from '../sections/Outro'
-import ReactPageScroller from 'react-page-scroller'
 
 const Home: NextPage = () => {
   return (
     <div className="font-body select-none">
-      <ReactPageScroller>
-        <Intro />
-        <Projects />
-        <Experience />
-        <Outro />
-      </ReactPageScroller>
+      <Intro />
     </div>
   )
 }
