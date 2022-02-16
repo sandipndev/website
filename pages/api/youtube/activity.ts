b368d2ecf09b75aef708323f95b95ebcb3034687
Sandipan Dey <hey@sandipan.dev>
2022-02-16 16:31:06 +0530

    feat: null youtube activity if greater than duration

--- pages/api/youtube/activity.ts
+++ pages/api/youtube/activity.ts
@@ -1,10 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
 import Redis from "ioredis";
+
+import { YoutubeActivity } from "./set";
 
 const handler = async (_: NextApiRequest, res: NextApiResponse) => {
   const redis = new Redis(process.env.REDIS_URL as string);
-  res.json(await redis.get("youtube-activity"));
+  const activity: YoutubeActivity | "" = JSON.parse(
+    (await redis.get("youtube-activity")) || ""
+  );
+
+  if (activity === "") res.json(null);
+  else if (Date.now() - activity.saveTime > activity.duration * 1000)
+    res.json(null);
+  else res.json(activity);
+
   redis.disconnect();
 };
 
